465e673a5d01d2fcfda75208ba9d4b00dc8308d8
nahdluddin <93822283+NAHDLUDDIN@users.noreply.github.com>
2021-12-19 15:15:45 +0700

    Update Saya_cinta_pemrograman.py

--- Saya_cinta_pemrograman.py
+++ Saya_cinta_pemrograman.py
@@ -1,11 +1,5 @@
-a=int(input("masukkan angka pertama: "))
-b=int(input("masukkan angka kedua: "))
-maks=0
-
-if a>b:
-    maks=a
-elif a==b:
-    print ('2 angka sama besar')
-else:
-    maks=b
-print ('angka terbesar adalah: ',maks)
+angka = 1
+while angka <=100:
+    print ('Saya cinta pemrograman-- baris ke',angka)
+    angka = angka + 1
+print ("selesai")
